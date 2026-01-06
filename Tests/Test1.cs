74b35f097201a78f7146f12874b286a50c45e4bc
Vyom Chamaria <chamarv@stpaulsschool.org.uk>
2026-01-06 17:36:44 +0000

    tests add and now working some error with some dulplicate attributes being created but now fixed in f1_simulation.csproj

---
+++ Tests/Test1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using F1_simulation;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1_ShouldPass()
+        {
+            // Example: simple math test
+            int a = 2;
+            int b = 3;
+            int sum = a + b;
+
+            Assert.AreEqual(5, sum, "2 + 3 should equal 5");
+        }
+    }
+}
